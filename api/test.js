73c781be4431d41d7da363e2f4cebc4bae7f5165
VladMch <musorovozich@gmail.com>
2024-10-15 19:51:17 +0300

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,12 +1,2 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/test', (req, res) => {
-    try {
-        res.json({ test: "success" });
-    } catch (error) {
-        res.status(500).json({ error: 'Ошибка сервера' });
-    }
-  });
-
-  module.exports = router;
+export default (request, response) => 
+  response.send('hello world');
